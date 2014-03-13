6268298246d223823d00623d13366f131c02b881
shirioko <maxkovaljov@outlook.com>
2014-03-13 23:02:06 +0100

    Added form for capturing tokens

---
+++ SnapSnatcher/frmCapture.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SnapSnatcher
+{
+    partial class frmCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCapture));
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(0, 0);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(273, 302);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // frmCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 302);
+            this.Controls.Add(this.txtOutput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCapture";
+            this.Text = "Capture auth_token";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmCapture_FormClosing);
+            this.Load += new System.EventHandler(this.frmCapture_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
---
+++ SnapSnatcher/frmCapture.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fiddler;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace SnapSnatcher
+{
+    public partial class frmCapture : Form
+    {
+        public string username;
+        public string authToken;
+
+        public frmCapture()
+        {
+            InitializeComponent();
+        }
+
+        protected byte[] GetCertificate()
+        {
+            X509Certificate2 cert = CertMaker.GetRootCertificate();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("-----BEGIN CERTIFICATE-----");
+            sb.AppendLine(Convert.ToBase64String(cert.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks));
+            sb.AppendLine("-----END CERTIFICATE-----");
+            return System.Text.Encoding.ASCII.GetBytes(sb.ToString());
+        }
+
+        private void frmCapture_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //check if root cert exists
+                if (!CertMaker.rootCertExists())
+                {
+                    //create root cert
+                    if (!CertMaker.createRootCert())
+                    {
+                        //oh shit nigga!
+                        throw new Exception("FiddlerCore error: could not create root certificate");
+                    }
+                }
+
+                //start fiddlercore
+                List<Session> oAllSessions = new List<Session>();
+
+                FiddlerApplication.BeforeRequest += delegate(Session oS)
+                {
+                    oS.bBufferResponse = true;
+                    Monitor.Enter(oAllSessions);
+                    oAllSessions.Add(oS);
+                    Monitor.Exit(oAllSessions);
+                };
+
+                FiddlerApplication.BeforeRequest += delegate(Session oS)
+                {
+                    this.AddMessage(string.Format("Captured request to {0}", oS.host));
+                    if (oS.host.Contains("snapcert"))
+                    {
+                        oS.utilCreateResponseAndBypassServer();
+                        this.AddMessage("Returning certificate");
+                        //return certificate, act as http server
+                        byte[] cert = this.GetCertificate();
+                        oS.responseBodyBytes = cert;
+                        oS.oResponse.headers["Content-Type"] = "application/x-x509-ca-cert";
+                        oS.oResponse.headers["Content-Length"] = cert.Length.ToString();
+                        oS.oResponse.headers["Connection"] = "Keep-Alive";
+                        oS.oResponse.headers.Add("Content-Disposition", "attachment; filename='fcore.cer'");
+                        oS.responseCode = 200;
+                    }
+                };
+
+                FiddlerApplication.BeforeResponse += delegate(Session oS)
+                {
+                    if (oS.host.Contains("feelinsonice-hrd.appspot.com"))
+                    {
+                        //try to capture req token
+                        this.AddMessage("Captured relevant request, processing...");
+                        try
+                        {
+                            oS.utilDecodeRequest();
+                            oS.utilDecodeResponse();
+
+                            JObject obj = JObject.Parse(oS.GetResponseBodyAsString());
+                            JToken tok = obj["auth_token"];
+                            JToken uname = obj["username"];
+                            if (tok != null && uname != null)
+                            {
+                                this.authToken = tok.ToObject<string>();
+                                this.username = uname.ToObject<string>();
+                                this.OnSuccess();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            this.AddMessage(string.Format("Error in handler: {0}", ex.ToString()));
+                        }
+                    }
+                };
+                CONFIG.IgnoreServerCertErrors = true;
+                FiddlerApplication.Startup(8877, false, true, true);
+                this.AddMessage("Proxy created on port 8877");
+                this.AddMessage(string.Format("Set the proxy on your mobile device to {0}:8877", this.GetIP()));
+                this.AddMessage("Listening...");
+            }
+            catch (Exception ex)
+            {
+                this.AddMessage(ex.Message);
+            }
+        }
+
+        protected string GetIP()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            if (localIPs.Length > 0)
+            {
+                List<string> v4ips = new List<string>();
+                foreach (IPAddress addr in localIPs)
+                {
+                    if (!addr.IsIPv6LinkLocal)
+                    {
+                        v4ips.Add(addr.ToString());
+                    }
+                }
+
+                if (v4ips.Count > 0)
+                {
+                    if (v4ips.Count > 1)
+                    {
+                        this.AddMessage("Warning: found multiple addresses, you might need to try a different one to work");
+                        foreach (string address in v4ips)
+                        {
+                            this.AddMessage(string.Format("\t{0}", address));
+                        }
+                    }
+                    return v4ips.FirstOrDefault();
+                }
+            }
+            throw new Exception("ERROR: No local IP v4 addresses found");
+        }
+
+        protected void OnSuccess()
+        {
+            //yay!
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Close();
+        }
+
+        protected delegate void StringDelegate(string message);
+        protected void AddMessage(string message)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    StringDelegate d = new StringDelegate(AddMessage);
+                    this.Invoke(d, message);
+                }
+                else
+                {
+                    this.txtOutput.Text += string.Format("{0}\r\n", message);
+                }
+            }
+            catch (Exception)
+            { }
+        }
+
+        protected void DoExit()
+        {
+            //disable proxy
+            this.AddMessage("Shutting down proxy...");
+            FiddlerApplication.Shutdown();
+            Thread.Sleep(1000);
+        }
+
+        private void frmCapture_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DoExit();
+        }
+    }
+}
